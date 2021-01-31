8e0366190253ebc5c4a1c68e21911a2c070c945e
Jens Johnson <jensj27@gmail.com>
2021-01-31 12:54:15 -0800

    added anti-join to search unmatched records

--- CWR_Canadian_species_filter.R
+++ CWR_Canadian_species_filter.R
@@ -1,17 +1,24 @@
+library(tidyverse)
+
 # Full US CWR Inventory from PNAS 2020 Table S1
 US_inventory <- read.csv("pnas_tableS1_Usinventory_1A_1B_1C.csv")
 # US CWR list from GRIN filter
-US_GRIN <- read.csv("All_US_CWR_Pri_Sec_Tert.csv")
+US_GRIN <- read.csv("GRIN_US_cleaned_data.csv")
 # Canada CWR list from GRIN filter
-Canada_GRIN <- read.csv("All_Canadian_CWR_Pri_Sec_Tert.csv")
+Canada_GRIN <- read.csv("GRIN_Canada_cleaned_data.csv")
 
-nrow(US_GRIN) # 1025 rows
+nrow(US_GRIN) # 502 rows
 nrow(US_inventory) # 656 rows
 # The GRIN US filter should give the same species list as the US inventory
-# However, the GRIN file has many more entries. In the GRIN file,
-# a species is listed multiple times if it is related to more than 1 crop
-# Is this the only reason the length is longer, or are there any species that 
-# were not covered in the PNAS inventory?
-
+# However, the GRIN file has fewer entries. 
 # are there any species in the GRIN US filter that are not in the PNAS supp?
 # any in the PNAS supp that are not in the GRIN filter?
+
+# First, figure out which column to compare (want to find unmatched species names)
+str(US_GRIN) # species name column is "CROP.WILD.RELATIVE"
+str(US_inventory) # # species name column is "Taxon"
+US_inventory <- rename(US_inventory, CROP.WILD.RELATIVE = Taxon) # change "Taxon" to ""
+US_GRIN$CROP.WILD.RELATIVE <- trimws(US_GRIN$CROP.WILD.RELATIVE, which = c("right")) # remove trailing white space on species names
+
+unmatched_records_from_US_GRIN <- anti_join(US_GRIN, US_inventory, by="CROP.WILD.RELATIVE")
+unmatched_records_from_US_Inventory <- anti_join(US_inventory, US_GRIN, by="CROP.WILD.RELATIVE")
