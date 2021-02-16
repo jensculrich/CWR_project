c08461a97d3a5c594de2d9d987b226fa14b3a746
Jens Johnson <jensj27@gmail.com>
2021-02-16 14:44:52 -0800

    added notes for next steps

--- CWR_Canada_Davidson_and_GRIN_taxa.R
+++ CWR_Canada_Davidson_and_GRIN_taxa.R
@@ -24,7 +24,21 @@ Canada_GRIN$CROP.WILD.RELATIVE <- trimws(Canada_GRIN$CROP.WILD.RELATIVE, which =
 # just different way of using the common name eg "raspberry" v "raspberry, red"
 CWR_Master_list <- full_join(Canada_Davidson, Canada_GRIN, by = "CROP.WILD.RELATIVE")
 
+# Re-read csv. I manually collapsed "Crop" and "CROP"
+df2 <- read.csv("CWR_Master_list.csv")
 
-# Next Remove duplicates caused by subspecies discrepancies. Will do this 
-# manually to be able to see where this occurs
-write.csv(CWR_Master_list, "CWR_Master_list.csv")
+# Things to do:
+# 1) Join with PNAS full data set to see if we can get category 1A, 1B statuses for taxa from 
+# Davidson list that were not in the GRIN list. Assign rest to category 1C? Need to 
+# check that they are indeed in same genus as crop?
+
+# 2) Join with PNAS full data set to see if we can add multiple crops when applicable for 
+# each CWR. E.g. if a species is a CWR of peach and plum, we want the column "Crop" to include both
+
+# 3) Begin adding additional info for each CWR for filtering and back-end data summary,
+# e.g. category for food crop specific, tree, ornamental, wild-utilized.
+# e.g. conservation status when applicable.
+# e.g. nut, fruit, vegetable, flower
+# Look to PNAS inventory and Tara's UBC summary for ideas.
+
+# 4) eventually intergate with geographic range data, and accession data from gardens/repository.
